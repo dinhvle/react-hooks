f16e00fdb7c07d930ba3154254e84ff811b6e21a
Dinh Le <dinh.v.le@gmail.com>
2021-02-06 00:47:29 -0600

    add useState, trying to understand React hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import Accordion from "./compnents/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript library",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use react by creating component",
+  },
+];
+
 const App = () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
 
 export default App;
