a4907db4668a850bb6f9c7fc306c088f0d55cf77
Dinh Le <dinh.v.le@gmail.com>
2021-02-07 23:46:25 -0600

    make axios request from useEffect

--- src/compnents/Search.js
+++ src/compnents/Search.js
@@ -1,5 +1,40 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 const Search = () => {
-  return <h1>Search Comp</h1>;
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    search();
+  }, [term]);
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label>Enter Search Term</label>
+        <input
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          className="input"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
