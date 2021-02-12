d6c8a3aea1cbe88952dd2fbea5241e93278097f2
Dinh Le <dinh.v.le@gmail.com>
2021-02-11 22:02:35 -0600

    use useRef to determine if user click on form or not

--- src/compnents/Dropdown.js
+++ src/compnents/Dropdown.js
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    });
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -20,7 +30,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
   return (
     <div>
-      <div className="ui form">
+      <div ref={ref} className="ui form">
         <div className="field">
           <label className="label">Select a Color</label>
           <div
